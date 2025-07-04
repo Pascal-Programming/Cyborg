b375bc01c8f486c7d57c35f2283cbe834b0ee9ae
Pascal Roth <p.roth@die-lobby.de>
2025-07-04 19:50:28 +0200

    [TASK] Quick save

--- src/Form/Type/CyborgTestType.php
+++ src/Form/Type/CyborgTestType.php
@@ -2,26 +2,34 @@
 
 namespace App\Form\Type;
 
+use Symfony\Bundle\FrameworkBundle\Translation\Translator;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class CyborgTestType extends AbstractType
 {
+    private function __construct(
+        private readonly Translator $translator,
+    )
+    {
+    }
+
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('cyborg1', CheckboxType::class, [
-                'label' => '1',
+                'label' => $this->translator->trans('cyborg.formLabel1'),
                 'required' => false,])
             ->add('cyborg2', CheckboxType::class, [
-                'label' => '2',
+                'label' => $this->translator->trans('cyborg.formLabel2'),
                 'required' => false,])
             ->add('cyborg3', CheckboxType::class, [
-                'label' => '3',
+                'label' => $this->translator->trans('cyborg.formLabel3'),
                 'required' => false,])
             ->add('cyborg4', CheckboxType::class, [
-                'label' => '4',
+                'label' => $this->translator->trans('cyborg.formLabel4'),
                 'required' => false,]);
 
     }
